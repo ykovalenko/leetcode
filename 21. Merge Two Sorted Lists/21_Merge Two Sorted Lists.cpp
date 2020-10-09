696a3ef9ffa08811133cc828892342d83c0beb02
yuri.kovalenko <yuri.kovalenko@gmail.com>
2020-10-10 00:42:41 +0200

    21. Merge Two Sorted Lists (https://leetcode.com/problems/merge-two-sorted-lists/)

---
+++ 21. Merge Two Sorted Lists/21_Merge Two Sorted Lists.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode* mergeTwoLists(ListNode* _l1, ListNode* _l2)
+  {
+    if (nullptr == _l1)
+      return _l2;
+    if (nullptr == _l2)
+      return _l1;
+
+    ListNode* answers = nullptr;
+    ListNode* answers_current = nullptr;
+
+    ListNode* l1 = _l1;
+    ListNode* l2 = _l2;
+    int value;
+    while (l1 != nullptr && l2 != nullptr)
+    {
+      if (l1->val < l2->val)
+      {
+        value = l1->val;
+        l1 = l1->next;
+      }
+      else
+      {
+        value = l2->val;
+        l2 = l2->next;
+      }
+
+      if (nullptr == answers)
+      {
+        answers = new ListNode(value);
+        answers_current = answers;
+      }
+      else
+      {
+        answers_current->next = new ListNode(value);
+        answers_current = answers_current->next;
+      }
+    }
+    
+    if (l1 != nullptr)
+    {
+      if (nullptr == answers)
+        answers = l1;
+      else
+        answers_current->next = l1;
+    }
+
+    if (l2 != nullptr)
+    {
+      if (nullptr == answers)
+        answers = l2;
+      else
+        answers_current->next = l2;
+    }
+
+    return answers;
+  }
+};
--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-"# leetcode" 
+"# leetcode"
+
+//-----------------------------------------------------------
+// Oct 2020
 
 2020.10.06:
-  69. Sqrt(x) (https://leetcode.com/problems/sqrtx/)
-  704. Binary Search (https://leetcode.com/problems/binary-search/)
+   69. Sqrt(x) (https://leetcode.com/problems/sqrtx/)
+   704. Binary Search (https://leetcode.com/problems/binary-search/)
 
 2020.10.07:
-  1. Two Sum (https://leetcode.com/problems/two-sum/)
-  33. Search in Rotated Sorted Array (https://leetcode.com/problems/search-in-rotated-sorted-array/)
+   1. Two Sum (https://leetcode.com/problems/two-sum/)
+   33. Search in Rotated Sorted Array (https://leetcode.com/problems/search-in-rotated-sorted-array/)
 
 2020.10.08:
-  1608. Special Array With X Elements Greater Than or Equal X (https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/)
+   1608. Special Array With X Elements Greater Than or Equal X (https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/)
+
+2020.10.09
+  21. Merge Two Sorted Lists (https://leetcode.com/problems/merge-two-sorted-lists/)
+
+//-----------------------------------------------------------
